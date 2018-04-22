b762228e52d1883a6e6ccda836911e62c04542c6
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-21 20:04:12 -0400

    Update kk.js

--- commands/kk.js
+++ commands/kk.js
@@ -3,11 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("NO! ADMIN ONLY! OMG!");
 	if (message.content === '^kk') {
-		message.guild.channels.filter(c => c.name === 'adbot-welcome-message').forEach(channel => {
-			channel.delete()
-  			.then(console.log)
-  			.catch(console.error);
-		});
+		message.guild.channels.filter(channel => channel.name === 'adbot-welcome-message').forEach(channel => channel.delete());
 		return message.channel.send(`\`All AdBot welcome channels are deleted.\``)
 }
 
