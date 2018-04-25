5d3f4736b49ad2009ddebda738ac65fa2bb43bfb
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-24 22:30:43 -0400

    Create description.js

---
+++ commands/description.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const ad = require("../adsave.json");
+
+exports.run = async (bot, message, args) => {
+  if(!ad[message.guild.id]) {
+    ad[message.guild.id] = {
+      desc: ""
+    }
+  }
+  const newDesc = args.join(" ");
+  if(!newDesc) return message.reply("please add the description you want me to send to other servers!\n\nUse: `^description <your server info>`\nExample: `^description This server has all the newest memes!`)")
+  if (message.content.toLowerCase().includes('@everyone')) {
+    message.reply("you can't tag people in your description!")
+    return;
+  }
+  if (message.content.toLowerCase().includes('@here')) {
+    message.reply("you can't tag people in your description!")
+    return;
+  }
+  ad[message.guild.id].desc = `${newDesc}`
+  
+  let botEmbed = new Discord.RichEmbed()
+  .setColor('#27ae60')
+  .setTitle('<:GWlegacyBlobThumbsUp:408674890175217664> UPDATE <:GWlegacyBlobThumbsUp:408674890175217664>')
+  .setDescription(`You have now updated your description! If you have already activated ads for your server, then it will now post the updated description!**__Description:__**\n${newDesc}`);
+  message.channel.send(botEmbed)
+}
+	      
+module.exports.help = {
+  name: "description"
+}
