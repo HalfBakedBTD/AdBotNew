2533d00ed5878fa94b221b168c2f7e9ca254d9de
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-06-28 15:15:46 -0400

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -2,14 +2,16 @@ module.exports.run = async (bot, message, args) => {
     let users = bot.guilds;
 
     let searchTerm = args[0];
-    if(!searchTerm) return message.channel.send("Please type a guild to search for!\n\nExample: `^search Fire Hut` will find and send servers with Fire Hut in thier name.");
-
-    let matches = users.filter(g => g.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    if(!searchTerm) return message.channel.send("Please type a guild to search for!\n\nExample: `^search Fire` will find and send servers with Fire in thier name.");
     
-    message.channel.send(`I am finding and messaging you servers with ${searchTerm} in thier name.`)
+    message.channel.send(`I am finding and direct messaging you servers with ${searchTerm} in thier name.`)
     
-    message.author.send(matches.map(g => g.name));
-
+    bot.guilds.filter(g => g.name.includes(`${searchTerm}`)).forEach(guild => {
+        message.channel.createInvite()
+      .then(invite => {
+        message.author.send(`**${guild.name}: https://discord.gg/${invite.code}**`);
+	  });
+    });
 }
     
 module.exports.help = {
