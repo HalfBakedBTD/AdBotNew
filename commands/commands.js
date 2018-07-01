2f3f824a856cfd5b42dfc2a0f9b2a29c495937b1
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-06-30 20:55:55 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -11,24 +11,14 @@ exports.run = async (bot, message, args) => {
   .setDescription("****Info Commands:****\n:question: **^help** - Shows some of my information.\n:grey_question: **^commands** - DMs you a list of my commands.\n:closed_book: **^test** - Helps you set up your server.");
 	message.author.send(botEmbed)
 	
-	let modEmbed = new Discord.RichEmbed()
-  .setColor('#4834d4')
-  .setDescription("e server.\n:recycle: **^recycle** - Clears messages. Equivilent to \`purge\` or \`clear\` on other bots.\n:cloud: **^purge-check** - Checks and tells you how many people will be pruned.\n:thunder_cloud_rain: **^purge** - Prunes members who have been inactive for a week.\n⚠ **^warn** - Warns a member in the server.");
-	//message.author.send(modEmbed)
-	
 	let adEmbed = new Discord.RichEmbed()
   .setColor('#eb4d4b')
   .setDescription("**__Main Commands:__**\n:arrow_heading_up: **^activate** - Bumps your server to the top of the advertising slot continuasly.\n:newspaper: **^desc** - Lets you make a description for your server. (`^desc` or `^description`)\n:mag_right: **^search** - lets you search servers that I am in by name.");
 	message.author.send(adEmbed)
 	
-	let infoEmbed = new Discord.RichEmbed()
-  .setColor('#be2edd')
-  .setDescription("**__Shorcuts:**\n:mailbox: **^info** - Gives various info about AdBot.\n:e_mail: **^invite** - Sends you and invite to add AdBot to your server.\n:pizza: **^server** - Links you to AdBot's official server.");
-	//message.author.send(infoEmbed)
-	
 	let linkEmbed = new Discord.RichEmbed()
   .setColor('#6ab04c')
-	.setDescription("**Helpful Links:**\n[Invite](https://discordapp.com/oauth2/authorize?client_id=421744026740457474&permissions=8&scope=bot) | [Discord](https://discord.gg/REkAA2X) | [Upvote](https://www.bing.com/images/search?q=comming+soon&FORM=HDRSC2)");
+	.setDescription("**Helpful Links:**\n[Invite](https://discordapp.com/oauth2/authorize?client_id=462783160128765952&permissions=8&scope=bot) | [Discord](https://discord.gg/REkAA2X) | [Upvote](https://www.bing.com/images/search?q=comming+soon&FORM=HDRSC2)");
 	message.author.send(linkEmbed)
 }
 	      
