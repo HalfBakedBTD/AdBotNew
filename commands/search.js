3efc655b7b64b57e0b7ec28937d092f7b127e53c
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-06-30 20:25:30 -0400

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
     let users = bot.guilds;
 	
-		let number = 1;
+		let number = 0;
 
     let searchTerm = args[0];
     if(!searchTerm) return message.channel.send("Please type a guild to search for!\n\nExample: `^search Fire` will find and send servers with Fire in thier name.");
@@ -9,9 +9,11 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(`I am finding and direct messaging you the top 3 servers with ${searchTerm} in thier name.`)
     
     bot.guilds.filter(g => g.id !== 'sgkmsrjgnrkkskf').forEach(guild => {
-			if (number !== 4) {
-	    	if (guild.name.toLowerCase().includes(`${searchTerm}`)) {
+			if (guild.name.toLowerCase().includes(`${searchTerm}`)) {
 					  number = number + 1
+	    	}
+			if (number < 3) {
+	    	if (guild.name.toLowerCase().includes(`${searchTerm}`)) {
         		message.channel.createInvite()
       				.then(invite => {
         				message.author.send(`**${guild.name}: https://discord.gg/${invite.code}**`);
@@ -19,7 +21,7 @@ module.exports.run = async (bot, message, args) => {
 	    	}
 			}
     });
-	  if (number > 1) {
+	  if (number > 0) {
 			message.channel.send(`I have found ${number} servers with ${searchTerm} in thier name!`)
 		} else {
 			message.channel.send(`I have found no servers with ${searchTerm} in thier name.`)
