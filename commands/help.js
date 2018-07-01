ca41ff2500e27ac8f3399e5d0badb907796f0dfb
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-06-30 20:45:31 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription("**AdBot** was made by <@284137818895417344> to help small servers grow. What is the best way to get noticed other than ads? Answer: there is none. We strive to get your server noticed!\n\n**QnA:**\nWhat do I do? Simply type `^commands` to pull up a list of commands!\nCan I get support? Yea! We have staff waiting on our [server](https://discord.gg/REkAA2X) to help you!\nCan I [donate](https://www.patreon.com/halfbakedpatreon)? Sure! All donations are a blessing!\n\n**Links:**\n[upvote](https://discordbots.org/bot/421744026740457474/vote) | [donate](https://www.patreon.com/halfbakedpatreon) | [server](https://discord.gg/REkAA2X)");
+  .setDescription("**AdBot** was made by <@284137818895417344> to help small servers grow. What is the best way to get noticed other than ads? Answer: there is none. We strive to get your server noticed!\n\n**QnA:**\nWhat do I do? Simply type `^commands` to pull up a list of commands!\nCan I get support? Yea! We have staff waiting on our [server](https://discord.gg/REkAA2X) to help you!\nCan I [donate](https://www.patreon.com/halfbakedpatreon)? Sure! All donations are welcome!\n\n**Links:**\n[upvote](https://www.bing.com/images/search?q=comming+soon&FORM=HDRSC2) | [donate](https://www.patreon.com/halfbakedpatreon) | [server](https://discord.gg/REkAA2X)");
   message.channel.send(botEmbed)
 }
 	      
