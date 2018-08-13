b7cfb728d5c4438aa6c1c7cacd9c0b1b3de4b89d
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-12 23:08:42 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 
-function adSend(bot, message) {
-
+exports.run = async (bot, message, args) => {
+//lol
+  
   message.channel.send(`If you made a channel called \`#ads\` then you are ready to \`^activate\`. Use \`^desc\` to make a description.`);
 }
 
