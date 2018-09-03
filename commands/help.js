bf7c7d4c277af1685cbad944e8bb9c99d3696a6c
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-03 16:58:21 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription("**AdBot** was made by <@284137818895417344> to help small servers grow. What is the best way to get noticed other than ads? Answer: there is none. We strive to get your server noticed!\n\n**QnA:**\nWhat do I do? Simply type `^commands` to pull up a list of commands!\nCan I get support? Yea! We have staff waiting on our [server](https://discord.gg/REkAA2X) to help you!\nCan I [donate](https://www.patreon.com/halfbakedpatreon)? Sure! All donations are welcome!\n\n**Links:**\n[upvote](https://www.bing.com/images/search?q=comming+soon&FORM=HDRSC2) | [donate](https://www.patreon.com/halfbakedpatreon) | [server](https://discord.gg/REkAA2X)");
+  .setDescription("I was made by <@284137818895417344> to help small servers grow. \"How?\" you might ask. Well the answer is simple. After you activate ads for your server, I send your ad to ALL other server I am in.\n\nTo prevent dead servers from constantly being advertised, we have a daily reset. Each day all data stored is deleted and if a server is active, they will activate ads again. If it is a dead server, they wont activate ads which roots them out leaving only active servers!\n\nDo `^commands` to get a list of commands.\n\n[donate](https://www.patreon.com/halfbakedpatreon) | [upvote](https://www.bing.com/images/search?q=comming+soon&FORM=HDRSC2) | [server](https://discord.gg/REkAA2X)");
   message.channel.send(botEmbed)
 }
 	      
