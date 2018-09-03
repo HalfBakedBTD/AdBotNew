0936797142ae3fa5ea202a589771e55b0532557c
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-03 16:28:57 -0400

    Update description.js

--- commands/description.js
+++ commands/description.js
@@ -3,10 +3,10 @@ const ad = require("../adsave.json");
 
 exports.run = async (bot, message, args) => {
 	if (message.author.id !== '346687165868015616') {
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No. Why would I do this for you? I have a **Admin only** policy.");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Only Admins can change the description.");
 	}
 	
-    if(!ad[message.guild.id]) return message.channel.send("Please use the command `^scan` to create your server file and cooldown before using this command. If you are in **AdBot Home:** https://discord.gg/UrEpuZR and use the `^scan` command, you ad will be posted every 30 minutes instead of every hour!");
+    if(!ad[message.guild.id]) return message.channel.send("Please use the command `^scan` to create your server file!");
   const newDesc = args.join(" ");
   if(!newDesc) return message.reply("please add the description you want me to send to other servers!\n\nUse: `^description <your server info>`\nExample: `^description This server has all the newest memes!`)")
   if (message.content.toLowerCase().includes('@everyone')) {
