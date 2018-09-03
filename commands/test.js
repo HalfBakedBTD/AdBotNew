f46f1ec75ad916f87e5d567db0be7139f23f4a72
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-03 17:08:56 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,14 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
-//lol
-  
-  message.channel.send(`If you made a channel called \`#ads\` then you are ready to \`^activate\`. Use \`^desc\` to make a description.`);
+  if (message.author.id !== '346687165868015616') {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("only Admins can test the setup of the server.");
+	}
+  let adschannel = message.guild.channels.find(`name`, "ads");
+  if(!adschannel) return message.channel.send("Hmm, I couldn't find a channel camed **#ads**. Please make one and use `^test` again.");
+  if(!ad[message.guild.id]) return message.channel.send("Please use the command `^scan` to create your server file! After you do so, type `^test` again.");
+  return message.channel.send("This server is all set up! You can `^activate` it now!");  
+  //message.channel.send(`If you made a channel called \`#ads\` then you are ready to \`^activate\`. Use \`^desc\` to make a description.`);
 }
 
 module.exports.help = {
