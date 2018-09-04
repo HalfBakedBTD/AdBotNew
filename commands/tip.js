02fe50a6b897fd61130e2d9f202acee897781c11
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-03 21:58:02 -0400

    Create tip.js

---
+++ commands/tip.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+  let botEmbed = new Discord.RichEmbed()
+  .setColor('#27ae60')
+  .setDescription("To grow your server faster, upvote the bot. Upvoting it puts it in featured where more people see it and then, add it. With more people adding it, your ad for your server goes to more people. This makes more see it and a bigger chance people will join.\n\nUpvote Link: https://discordbots.org/bot/462783160128765952");
+  message.channel.send(botEmbed)
+}
+	      
+module.exports.help = {
+  name: "tip"
+}
