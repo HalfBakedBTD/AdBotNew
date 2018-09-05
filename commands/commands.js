50a03ea218d0f185f9cb6cc0ba73e60519b00a2a
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-05 13:01:08 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {	
   let botEmbed = new Discord.RichEmbed()
   .setColor('#dff9fb')
-  .setDescription("**__My Commands:__**\n❔ **help** - shows information about me.\n📂 **commands** - shows a list of commands.\n🔎 **scan** - creates server files needed for activation.\n🚨 **test** - allowes you to see if your server is set up correctly.\n🏁 **activate** - allows you to share activate ad sending for your server.\n📰 **desc** or **description** - helps you create a good server description.\n📈 **stats** - shows bot stats!\n📨 **invite** - gives you a link to invite me to your server.\n\n**__New Commands:__**\n✏ **tip** - tells you how you can get even more members faster!")
+  .setDescription("**__My Commands:__**\n❔ **help** - shows information about me.\n📂 **commands** - shows a list of commands.\n🔎 **scan** - creates server files needed for activation.\n🚨 **test** - allowes you to see if your server is set up correctly.\n🏁 **activate** - allows you to share activate ad sending for your server.\n📰 **desc** - helps you create a good server description.\n📈 **stats** - shows bot stats!\n📨 **invite** - gives you a link to invite me to your server.\n\n**__New Commands:__**\n✏ **tip** - tells you how you can get even more members faster!\n**vote** - gives you a link to vote for me.")
 	.setFooter(`PREFIX: ^`);
 	message.channel.send(botEmbed)
 }
