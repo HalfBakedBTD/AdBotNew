360c279f0409a15f0bda07f650e41e7861934e9c
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-04 21:54:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+const DBL = require("dblapi.js");
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ2Mjc4MzE2MDEyODc2NTk1MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTM2MTEyMzIzfQ.vUIOdUSuw3hEHsqjNiOyifQALYELr4plSv7oHEhvS1w', bot);
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
@@ -25,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.channels.filter(c => c.name === 'ads').forEach(channel => {
-    channel.send(`I have restarted, please activate ads again.`);
+    channel.send(`I have restarted, please activate ads again.\nUse the \`^desc\` command to set a description of your server. More members will join!`);
   });
 
   bot.user.setGame(`^help | ^invite`);
