e96fc1a8d628337976cc5cd6f230e9140df3b6bf
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-07-24 11:33:04 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ const ad = require("../adsave.json");
 exports.run = async (bot, message, args) => {
 	let botEmbed = new Discord.RichEmbed()
   .setColor('#dff9fb')
-  .setDescription("🔗 **My Invite:** https://discordapp.com/oauth2/authorize?client_id=462783160128765952&permissions=8&scope=bot\n\n🏙 **My Server:** https://discord.gg/UrEpuZR");
+  .setDescription("🔗 **My Invite:** https://discordapp.com/oauth2/authorize?client_id=603604735424987160&permissions=8&scope=bot\n\n🏙 **My Server:** https://discord.gg/UrEpuZR");
 	message.channel.send(botEmbed)
 }
 	      
