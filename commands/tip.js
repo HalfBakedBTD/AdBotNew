1487ae55877b727468ee11d90f309510367de3b8
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-07-24 11:39:04 -0400

    Update tip.js

--- commands/tip.js
+++ commands/tip.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription("To grow your server faster, upvote the bot. Upvoting it puts it in featured where more people see it and then, add it. With more people adding it, your ad for your server goes to more people. This makes more see it and a bigger chance people will join.\n\nUpvote Link: https://discordbots.org/bot/462783160128765952");
+  .setDescription("To grow your server faster, upvote the bot. Upvoting it puts it in featured where more people see it and then, add it. With more people adding it, your ad for your server goes to more people. This makes more see it and a bigger chance people will join.\n\nUpvote Link: `Comming soon...`");
   message.channel.send(botEmbed)
 }
 	      
